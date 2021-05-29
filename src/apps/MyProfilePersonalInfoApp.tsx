6deae7c51fd26927b4f5f926f8caf47addade425
erikdao <cuong@neuraltalks.io>
2021-05-30 01:36:14 +0200

    Update: add more translation

--- src/apps/MyProfilePersonalInfoApp.tsx
+++ src/apps/MyProfilePersonalInfoApp.tsx
@@ -1,5 +1,6 @@
 /* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
 import { ChevronRightIcon } from '@heroicons/react/outline';
+import { Trans } from '@lingui/macro';
 import React, { createRef, useContext, useState } from 'react';
 import { Link } from 'react-router-dom';
 import AboutMeScene from 'src/assets/icons/aboutme_scene.png';
@@ -59,19 +60,25 @@ export function MyProfilePersonalInfoApp(): JSX.Element {
       <div className="flex-1 flex-grow h-full w-full flex flex-col overflow-x-hidden overflow-y-auto p-6 bg-white">
         <div className="flex-0 flex-shrink-0 flex-grow flex flex-col items-center w-full sm:w-3/4 max-w-7xl md:max-w-4xl mx-auto">
           <div className="flex-0 flex flex-col items-center justify-center p-4 space-y-4 pt-8 w-full">
-            <h1 className="font-normal text-3xl text-gray-700">Personal info</h1>
-            <p className="text-base text-gray-500">Basic info, like your name and photo, that you use on Google services</p>
+            <h1 className="font-normal text-3xl text-gray-700">
+              <Trans id="myprofile.personalinfo.pagetitle" />
+            </h1>
+            <p className="text-base text-gray-500">
+              <Trans id="myprofile.personalinfo.pagedescription" />
+            </p>
           </div>
 
           {/* Basic info */}
           <div className="rounded-lg border border-gray-300 w-full mt-2 divide-y divide-gray-200">
             <div className="p-4 divide-y-skip">
-              <h3 className="text-2xl text-gray-700">Basic info</h3>
-              <p className="text-sm mt-2 text-gray-500">Some info may be visible to other people using Google services. <a href="/" className="text-red-700">Learn more</a></p>
+              <h3 className="text-2xl text-gray-700"><Trans id="myprofile.personalinfo.basicinfotitle" /></h3>
+              <p className="text-sm mt-2 text-gray-500">
+                <Trans id="myprofile.personalinfo.basicinfodescription" /> <a href="/" className="text-red-700">Learn more</a>
+              </p>
             </div>
             <div className="p-4 flex items-stretch text-xs text-gray-500 hover:cursor-pointer hover:bg-gray-100">
-              <div className="flex-shrink-0 w-1/4 uppercase">Photo</div>
-              <div className="flex-grow w-2/4 text-sm">Click on the image and select a new file to update</div>
+              <div className="flex-shrink-0 w-1/4 uppercase"><Trans id="myprofile.personalinfo.photolabel" /></div>
+              <div className="flex-grow w-2/4 text-sm"><Trans id="myprofile.personalinfo.photodescription" /></div>
               <div className="flex-grow flex justify-end">
                 <img
                   src={authUser.photoURL}
@@ -85,7 +92,7 @@ export function MyProfilePersonalInfoApp(): JSX.Element {
             </div>
 
             <div className="p-4 flex items-stretch text-xs  hover:cursor-pointer hover:bg-gray-100">
-              <div className="flex-shrink-0 w-1/4 uppercase text-gray-500">Name</div>
+              <div className="flex-shrink-0 w-1/4 uppercase text-gray-500"><Trans id="myprofile.personalinfo.namelabel" /></div>
               <div className="flex-grow text-sm font-normal text-gray-700">{authUser.displayName}</div>
               <div className="flex-grow-0 flex-shrink-0 w-4 flex items-center justify-center">
                 <ChevronRightIcon
@@ -101,8 +108,8 @@ export function MyProfilePersonalInfoApp(): JSX.Element {
               onClick={() => setDialog(DialogType.MY_PROFILE_UPDATE_BIRTHDAY)}
               onKeyDown={() => setDialog(DialogType.MY_PROFILE_UPDATE_BIRTHDAY)}
             >
-              <div className="flex-shrink-0 w-1/4 uppercase text-gray-500">Birthday</div>
-              <div className="flex-grow text-sm font-normal text-gray-700">Add birthday</div>
+              <div className="flex-shrink-0 w-1/4 uppercase text-gray-500"><Trans id="myprofile.personalinfo.birthdaylabel" /></div>
+              <div className="flex-grow text-sm font-normal text-gray-700"><Trans id="myprofile.personalinfo.addbirthday" /></div>
               <div className="flex-grow-0 flex-shrink-0 w-4 flex items-center justify-center cursor-pointer">
                 <ChevronRightIcon
                   className="h-4 w-4 text-gray-700"
@@ -112,7 +119,7 @@ export function MyProfilePersonalInfoApp(): JSX.Element {
             </div>
 
             <div className="p-4 flex items-stretch text-xs  hover:cursor-pointer hover:bg-gray-100">
-              <div className="flex-shrink-0 w-1/4 uppercase text-gray-500">Password</div>
+              <div className="flex-shrink-0 w-1/4 uppercase text-gray-500"><Trans id="myprofile.personalinfo.passwordlabel" /></div>
               <div className="flex-grow text-sm font-normal text-gray-700 flex flex-col">
                 <div>••••••••</div>
                 <div className="text-sm text-gray-500">Last changed May 11, 2019</div>
--- src/config/base.ts
+++ src/config/base.ts
@@ -12,8 +12,6 @@ export type TFirebaseConfig = {
   measurementId?: string;
 };
 
-console.log(process.env.REACT_APP_NODE_ENV, process.env.REACT_APP_API_KEY);
-
 export const firebaseConfig: TFirebaseConfig = {
   apiKey: process.env.REACT_APP_API_KEY,
   authDomain: process.env.REACT_APP_AUTH_DOMAIN,
